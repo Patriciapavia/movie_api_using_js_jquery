5f0c50e1a19915983d7af6d7fe057743ddb4625c
patricia <patriciapavia236@gmail.com>
2020-07-01 16:39:08 +1000

    fixed http

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function getMovie() {
   let movieId = sessionStorage.getItem("movieId");
 
   axios
-    .get(`http://www.omdbapi.com/?i=${movieId}&apikey=5bb03fee`)
+    .get(`https://www.omdbapi.com/?i=${movieId}&apikey=5bb03fee`)
     .then((response) => {
       console.log(response);
       let movie = response.data;
@@ -75,7 +75,7 @@ function getMovie() {
               <h3>Plot</h3>
               ${movie.Plot}
               <hr>
-              <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+              <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
               <a href="index.html" class="btn btn-default">Go Back To Search</a>
             </div>
           </div>
